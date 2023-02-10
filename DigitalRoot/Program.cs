d37b4e5c89b2a129a5cc5364aec0f04d0f2dc66f
Ilya <Ilya@DESKTOP-GP2NNM6>
2023-02-10 20:22:10 +0400

    Добавьте файлы проекта.

---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DigitalRoot
+{
+    internal class Program
+    {
+        static int DigitalRoot(long n)
+        {
+            while (n > 9)
+            {
+                long sum = 0;
+
+                while (n != 0)
+                {
+                    sum += n % 10;
+                    n /= 10;
+                }
+
+                n = sum;
+            }
+
+            string a = n.ToString();
+            int b = Int32.Parse(a);
+
+            return b;
+        }
+        static void Main(string[] args)
+        {
+            DigitalRoot(999999999999); // 9
+        }
+    }
+}
